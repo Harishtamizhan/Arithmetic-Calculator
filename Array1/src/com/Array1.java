eb2361326754e0ccde69de422ea2bef39fcdf1f0
your Name <mjhy2000@gmail.com>
2022-08-01 15:22:13 +0530

    Array1

---
+++ Array1/src/com/Array1.java
@@ -0,0 +1,40 @@
+package com;
+
+import java.util.Scanner;
+
+public class Array1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Array");
+		int[] a=new int[4];
+		Scanner s=new Scanner(System.in);
+		a[0]=20;
+		a[1]=40;
+		a[2]=60;
+		a[3]=80;
+		
+		int length = a.length;
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+		System.out.println("Multi-Dimentional Array");
+		
+		int b[][]  = {{9, 4, 6 },
+				      {3, 6, 9},
+				      {2,6,8 }};
+	      
+	    
+	    int length1 =b.length;
+	    System.out.println("Printing the value using for loop");
+	    for (int i=0;i<b.length;i++) {
+	    	for(int j=0;j<b[i].length;j++) {
+	    		System.out.println(b[i][j]);
+	    	}
+	    }
+	    
+	    
+	    
+	 }
+
+}
